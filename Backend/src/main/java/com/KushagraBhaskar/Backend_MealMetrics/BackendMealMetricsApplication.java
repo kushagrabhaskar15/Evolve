05f2b679bd1f597801c4047d574aa717986b92a4
kushagrabhaskar15 <kushagrabhaskar05@gmail.com>
2025-09-27 21:06:33 +0530

    adding necessary files (initial commit)

---
+++ Backend/src/main/java/com/KushagraBhaskar/Backend_MealMetrics/BackendMealMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.KushagraBhaskar.Backend_MealMetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMealMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMealMetricsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/KushagraBhaskar/Backend_MealMetrics/BackendMealMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KushagraBhaskar.Backend_MealMetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMealMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
